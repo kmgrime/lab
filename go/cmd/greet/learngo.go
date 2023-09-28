a82929d9b8ac1758522b45754f9616543e650dcb
u58306 <u58306@N54959X.energycorp.com>
2023-09-28 12:25:51 +0200

    re-structure

--- go/cmd/greet/learngo.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import learngo "github.com/kmgrime/lab/go"
-
-func main() {
-	learngo.Greet()
-}
--- go/main.go
+++ go/main.go
@@ -3,5 +3,6 @@ package learngo
 import "fmt"
 
 func Greet() {
-	fmt.Println("Hello, friend.")
+	fmt.Println("Hello there, make go fun!")
+	fmt.Println("Tutorials under go_tutorials folder.")
 }
